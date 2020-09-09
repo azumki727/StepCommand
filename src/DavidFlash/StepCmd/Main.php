a9bb11bc7fe158ff14b75e2466b3de31770a94c1
DavidFlashCZ <59114959+DavidFlashCZ@users.noreply.github.com>
2020-09-09 17:05:37 +0200

    Mutli-World Support Added
    Check in config

--- src/DavidFlash/StepCmd/Main.php
+++ src/DavidFlash/StepCmd/Main.php
@@ -11,9 +11,11 @@ use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\entity\EntityLevelChangeEvent;
 use pocketmine\block\BlockFactory;
 use pocketmine\block\Block;
 use pocketmine\event\Listener;
+use pocketmine\level\Level;
 
 
 class Main extends PluginBase implements Listener {
@@ -23,19 +25,32 @@ class Main extends PluginBase implements Listener {
           if(!is_file($this->getDataFolder() . "config.yml")){
 			        $this->saveResource('/config.yml');
 		      }
-        }
+    }
 
     public function on_move(PlayerMoveEvent $event){
         $player = $event->getPlayer();
         $playername = $player->getName();
         $block = $player->getLevel()->getBlock($player->subtract(0, 1, 0));
 
+    if($this->getConfig()->getNested("multiworld-enabled")==true){
+      if($player->getLevel()->getName() === $this->getConfig()->getNested("world")){
       if($block->getId()==$this->getConfig()->getNested("block-id")){
         if($player->hasPermission("stepcommand.bypass")){}
         else if($this->getConfig()->get("executor") === "console"){
           $this->getServer()->dispatchCommand(new ConsoleCommandSender, $this->getConfig()->getNested("command"));
         }else{
           $this->getServer()->dispatchCommand($player, $this->getConfig()->getNested("command"));
+          }
+        }
+      }
+    }else{
+      if($block->getId()==$this->getConfig()->getNested("block-id")){
+        if($player->hasPermission("stepcommand.bypass")){}
+        else if($this->getConfig()->get("executor") === "console"){
+          $this->getServer()->dispatchCommand(new ConsoleCommandSender, $this->getConfig()->getNested("command"));
+        }else{
+          $this->getServer()->dispatchCommand($player, $this->getConfig()->getNested("command"));
+          }
         }
       }
     }
