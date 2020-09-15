b9a9bb9a6fcb164c4efe8168e3f246cb56522222
DavidFlashCZ <59114959+DavidFlashCZ@users.noreply.github.com>
2020-09-15 14:52:45 +0200

    Added {player} and {world}
    Use {player} for player name and {world} for world the player is in.

--- src/DavidFlash/StepCmd/Main.php
+++ src/DavidFlash/StepCmd/Main.php
@@ -30,16 +30,21 @@ class Main extends PluginBase implements Listener {
     public function on_move(PlayerMoveEvent $event){
         $player = $event->getPlayer();
         $playername = $player->getName();
+        $playerworld = $player->getLevel()->getName();
         $block = $player->getLevel()->getBlock($player->subtract(0, 1, 0));
+      $phrase  = $this->getConfig()->getNested("command");
+      $toreplace = array("{player}", "{world}");
+      $replaced = array($playername, $playerworld);
+      $command = str_replace($toreplace, $replaced, $phrase);
 
     if($this->getConfig()->getNested("multiworld-enabled")==true){
       if($player->getLevel()->getName() === $this->getConfig()->getNested("world")){
       if($block->getId()==$this->getConfig()->getNested("block-id")){
         if($player->hasPermission("stepcommand.bypass")){}
         else if($this->getConfig()->get("executor") === "console"){
-          $this->getServer()->dispatchCommand(new ConsoleCommandSender, $this->getConfig()->getNested("command"));
+          $this->getServer()->dispatchCommand(new ConsoleCommandSender, $command);
         }else{
-          $this->getServer()->dispatchCommand($player, $this->getConfig()->getNested("command"));
+          $this->getServer()->dispatchCommand($player, $command);
           }
         }
       }
@@ -47,9 +52,9 @@ class Main extends PluginBase implements Listener {
       if($block->getId()==$this->getConfig()->getNested("block-id")){
         if($player->hasPermission("stepcommand.bypass")){}
         else if($this->getConfig()->get("executor") === "console"){
-          $this->getServer()->dispatchCommand(new ConsoleCommandSender, $this->getConfig()->getNested("command"));
+          $this->getServer()->dispatchCommand(new ConsoleCommandSender, $command);
         }else{
-          $this->getServer()->dispatchCommand($player, $this->getConfig()->getNested("command"));
+          $this->getServer()->dispatchCommand($player, $command);
           }
         }
       }
